cf896d3afc470fe7aa03c3e11d20e3425bd503e9
olaakintola <olanipekun_akin@yahoo.co.uk>
2020-12-31 14:37:37 +0000

    commit, before pulling new changes

--- client/src/main/java/client/Client.java
+++ client/src/main/java/client/Client.java
@@ -17,24 +17,14 @@ import service.core.HotelRequest;
 public class Client {
 	
 	public static final String newArgs = "http://localhost:8081/bookings";
-<<<<<<< HEAD
-//	public static final String winArgs = "http://localhost:192.168.99.100:8081/bookings";
-=======
-<<<<<<< HEAD
-	public static int referenceNumber = 0;
-=======
-	public static final String winArgs = "http://localhost:192.168.99.100:8081/bookings";
->>>>>>> 8f62162aa1b92e62304b3330572f2dd31b054420
 
-	
->>>>>>> fd57eaac6da07b9fe0460088a37ed211b687d78d
+	public static int referenceNumber = 0;
 	
 	public static void main(String[] args) {
 		
-	// public static void bookingAdventure(ClientBooking[] clients) {	
+//	 public static void bookingAdventure(ClientBooking[] clients) {	
 		Flight[] flights = new Flight[10];
 		RestTemplate restTemplate = new RestTemplate();
-<<<<<<< HEAD
 
 		ClientBooking clientBooking = new ClientBooking("Donald Trump", "Dublin", "Ireland", "Paris", "France", false,
 				 "2021-01-09", "2021-01-17", "EUR");
@@ -49,33 +39,12 @@ public class Client {
 		referenceNumber++;
 		clientRequest.setReferenceNumber(referenceNumber);
 
-
 			HttpEntity<ClientRequest> request = new HttpEntity<>(clientRequest);
-=======
-		for (ClientBooking booking : clients){
-			HttpEntity<ClientBooking> request = new HttpEntity<>(booking);
 
->>>>>>> fd57eaac6da07b9fe0460088a37ed211b687d78d
-			
-<<<<<<< HEAD
-//			if(SystemUtils.IS_OS_WINDOWS){
-//				flights = restTemplate.postForObject(winArgs,request,Flight[].class);
-//			}
-//			else{
+//		for (ClientBooking booking : clients){
+//			HttpEntity<ClientBooking> request = new HttpEntity<>(booking);
+
 				flights = restTemplate.postForObject(newArgs,request,Flight[].class);
-
-//			}
-=======
-			// if(SystemUtils.IS_OS_WINDOWS){
-			// 	flights = restTemplate.postForObject(winArgs,request,Flight[].class);
-			// }
-			// else{
-				flights = restTemplate.postForObject(newArgs,request,Flight[].class);
-
-			// }
->>>>>>> 8f62162aa1b92e62304b3330572f2dd31b054420
-			// flights = restTemplate.postForObject("http://localhost:8081/bookings",request,Flight[].class);
-
 
 			for (int i=0; i < flights.length; i++){
 				Flight f = flights[i];
@@ -84,8 +53,6 @@ public class Client {
 					System.out.println("Price of flight is: " + f.getPrice());
 				}	
 			}
-
-<<<<<<< HEAD
 		/**
 		 *  Barry's testing code below
 		 *  */ 
@@ -97,14 +64,11 @@ public class Client {
 		//   HttpEntity<Integer> request2 = new HttpEntity<>(767);
 		//   restTemplate.put(newArgs+"/1",request2);
 		 
-		 
-=======
 		/*
 		 * HttpEntity<String> request = new HttpEntity<>("Johnson");
 		 * 
 		 * restTemplate.put(args[0],request);
 		 */
->>>>>>> fd57eaac6da07b9fe0460088a37ed211b687d78d
 	} 
    
 	// /**
@@ -145,4 +109,6 @@ public class Client {
 	 * Test Data
 	 */
 	
+
 }
+
--- client/src/main/java/client/ClientController.java
+++ client/src/main/java/client/ClientController.java
@@ -1,59 +1,96 @@
-// package client;
+package client;
 
-// import java.io.IOException;
-// import java.util.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
 
-// import javax.servlet.http.HttpServletResponse;
-// import service.core.ClientBooking;
-// import client.Client;
+import javax.servlet.http.HttpServletResponse;
+import service.core.ClientBooking;
+import service.core.HotelRequest;
+import client.Client;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Controller;
-// import org.springframework.ui.Model;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
-// import org.springframework.web.bind.annotation.RequestMethod;
-// import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 
-// /**
-//  * Implementation of the AuldFellas insurance quotation service.
-//  * 
-//  * @author Rem
-//  *
-//  */
-// @Controller
-// public class ClientController { 
+/**
+ * Implementation of the AuldFellas insurance quotation service.
+ * 
+ * @author Rem
+ *
+ */
+@Controller
+public class ClientController { 
+    private HashMap<String, String> cityCodes = new HashMap<String, String>();
+    private ClientBooking clientBooking = new ClientBooking();
+    private HotelRequest hotelRequest = new HotelRequest();
+//	@RequestMapping(value="/",method=RequestMethod.GET)
+//	@ResponseBody 
+	@GetMapping("/")
+	public String greeting(){
+		return "index.html";
+	}
 	
-// //	@RequestMapping(value="/",method=RequestMethod.GET)
-// //	@ResponseBody 
-// 	@GetMapping("/")
-// 	public String greeting(){
-// 		return "index.html";
-// 	}
+	@GetMapping("/hotels")
+	public String hotelsForm(){
+		return "hotels.html";
+	}
+	
 
-// 	@RequestMapping(value="/processForm",method=RequestMethod.POST)  
-// 	public void processForm(String name, String cityOfOrigin, String countryOfOrigin, String cityOfDestination, String countryOfDestination, boolean oneWayTrip, String returnDate, String outboundDate, String currency, HttpServletResponse response) throws IOException {
+	@RequestMapping(value="/processFlightsForm",method=RequestMethod.POST)  
+	public void processFlightsForm(String name, String cityOfOrigin, String countryOfOrigin, String cityOfDestination, String countryOfDestination, boolean oneWayTrip, String returnDate, String outboundDate, String currency, HttpServletResponse response) throws IOException {
 
-// 			ClientBooking[] clientArray = new ClientBooking[1] ;
-// 			ClientBooking clientBooking = new ClientBooking();
-// 			clientBooking.setName(name);
-// 			clientBooking.setCityOfOrigin(cityOfOrigin);
-// 			clientBooking.setCountryOfOrigin(countryOfOrigin);
-// 			clientBooking.setCityOfDestination(cityOfDestination);
-// 			clientBooking.setCountryOfDestination(countryOfDestination);
-// 			clientBooking.setOneWayTrip(oneWayTrip);
-// 			clientBooking.setReturnDate(returnDate);
-// 			clientBooking.setOutboundDate(outboundDate);
-// 			clientBooking.setCurrency(currency);
-// 			clientArray[0] = clientBooking;
-// 			Client.bookingAdventure(clientArray);
-// 			response.sendRedirect("/");
-// 	}
-// }
+			// ClientBooking[] clientArray = new ClientBooking[1] ;
+			// ClientBooking clientBooking = new ClientBooking();
+			clientBooking.setName(name);
+			clientBooking.setCityOfOrigin(cityOfOrigin);
+			clientBooking.setCountryOfOrigin(countryOfOrigin);
+			clientBooking.setCityOfDestination(cityOfDestination);
+			clientBooking.setCountryOfDestination(countryOfDestination);
+			clientBooking.setOneWayTrip(oneWayTrip);
+			clientBooking.setReturnDate(returnDate);
+			clientBooking.setOutboundDate(outboundDate);
+			clientBooking.setCurrency(currency);
+            // clientArray[0] = clientBooking;
+            cityCodeGenerator();
+			response.sendRedirect("/hotels");
+    }
+    
+    private void cityCodeGenerator(){
+        Scanner sc = new Scanner("city_codes.txt");
+        while(sc.hasNextLine()) 
+        {   
+            String line = sc.nextLine();
+            int index = line.lastIndexOf(" ")+1;
+            String cityCode = line.substring(index);
+            String cityName = line.substring(0,index);
+	        cityCodes.put(cityName.toLowerCase() , cityCode );
+        }
+    }
+
+    @RequestMapping(value="/processHotelsForm",method=RequestMethod.POST)
+    public void processHotelsForm(String location, String rooms, String checkIn, String checkOut, String minRating, HttpServletResponse response) throws IOException
+    {
+        if(cityCodes.get(location.toLowerCase())==null){
+            PrintWriter out = response.getWriter();
+            out.println("<script>");
+            out.println("alert('" + "invalid city entered, enter again" + "');");
+            out.println("window.location.replace('" + "/hotels" + "');");
+            out.println("</script>");
+        }else {
+			response.sendRedirect("/");
+        }
+        
+    }  
+}
 
 
 
