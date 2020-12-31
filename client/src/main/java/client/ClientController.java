19cf8973b70ec38f00372471a3af67551bc32b58
Barry <barry.murphy@ucdconnect.ie>
2020-12-31 16:10:24 +0000

    Test

--- client/src/main/java/client/ClientController.java
+++ client/src/main/java/client/ClientController.java
@@ -1,60 +1,60 @@
-// package client;
+package client;
 
-// import java.io.IOException;
-// import java.util.*;
+import java.io.IOException;
+import java.util.*;
 
-// import javax.servlet.http.HttpServletResponse;
-// import service.core.ClientBooking;
-// import client.Client;
+import javax.servlet.http.HttpServletResponse;
+import service.core.ClientBooking;
+import client.Client;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Controller;
-// import org.springframework.ui.Model;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
-// import org.springframework.web.bind.annotation.RequestMethod;
-// import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 
-// /**
-//  * Implementation of the AuldFellas insurance quotation service.
-//  * 
-//  * @author Rem
-//  *
-//  */
-// @Controller
-// public class ClientController { 
+/**
+ * Implementation of the AuldFellas insurance quotation service.
+ * 
+ * @author Rem
+ *
+ */
+@Controller
+public class ClientController { 
 	
-// //	@RequestMapping(value="/",method=RequestMethod.GET)
-// //	@ResponseBody 
-// 	@GetMapping("/")
-// 	public String greeting(){
-// 		return "index.html";
-// 	}
+//	@RequestMapping(value="/",method=RequestMethod.GET)
+//	@ResponseBody 
+	@GetMapping("/")
+	public String greeting(){
+		return "index.html";
+	}
 
-// 	@RequestMapping(value="/processForm",method=RequestMethod.POST)  
-// 	public void processForm(String name, String cityOfOrigin, String countryOfOrigin, String cityOfDestination, String countryOfDestination, boolean oneWayTrip, String returnDate, String outboundDate, String currency, HttpServletResponse response) throws IOException {
+	@RequestMapping(value="/processForm",method=RequestMethod.POST)  
+	public void processForm(String name, String cityOfOrigin, String countryOfOrigin, String cityOfDestination, String countryOfDestination, boolean oneWayTrip, String returnDate, String outboundDate, String currency, HttpServletResponse response) throws IOException {
 
-// 			ClientBooking[] clientArray = new ClientBooking[1] ;
-// 			ClientBooking clientBooking = new ClientBooking();
-// 			clientBooking.setName(name);
-// 			clientBooking.setCityOfOrigin(cityOfOrigin);
-// 			clientBooking.setCountryOfOrigin(countryOfOrigin);
-// 			clientBooking.setCityOfDestination(cityOfDestination);
-// 			clientBooking.setCountryOfDestination(countryOfDestination);
-// 			clientBooking.setOneWayTrip(oneWayTrip);
-// 			clientBooking.setReturnDate(returnDate);
-// 			clientBooking.setOutboundDate(outboundDate);
-// 			clientBooking.setCurrency(currency);
-// 			clientArray[0] = clientBooking;
-// 			Client.bookingAdventure(clientArray);
-// 			response.sendRedirect("/");
-// 	}
-// }
-// 
+			ClientBooking[] clientArray = new ClientBooking[1] ;
+			ClientBooking clientBooking = new ClientBooking();
+			clientBooking.setName(name);
+			clientBooking.setCityOfOrigin(cityOfOrigin);
+			clientBooking.setCountryOfOrigin(countryOfOrigin);
+			clientBooking.setCityOfDestination(cityOfDestination);
+			clientBooking.setCountryOfDestination(countryOfDestination);
+			clientBooking.setOneWayTrip(oneWayTrip);
+			clientBooking.setReturnDate(returnDate);
+			clientBooking.setOutboundDate(outboundDate);
+			clientBooking.setCurrency(currency);
+			clientArray[0] = clientBooking;
+			Client.bookingAdventure(clientArray);
+			response.sendRedirect("/");
+	}
+}
+
 
 
 
